584a0c12e4702092aa1938465db4709ae3d91a13
denisiuk_sergey <seruy722@gmail.com>
2018-11-15 16:43:02 +0200

    add migration, model and controller for cargo page

---
+++ database/migrations/2018_11_15_141854_create_cargos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCargosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cargos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->float('sum', 8, 2)->default(0);
+            $table->string('client');
+            $table->integer('place')->default(0)->unsigned();
+            $table->float('kg', 8, 2)->default(0)->unsigned();
+            $table->string('fax')->nullable();
+            $table->string('notation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cargos');
+    }
+}
